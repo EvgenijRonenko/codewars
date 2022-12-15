48675612e685f55bf68b147fea390cfb80d7fc16
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-12-15 21:28:00 +0100

    62 codewars done!

--- 07.js
+++ 07.js
@@ -2,3 +2,14 @@
 function greet(name){
     return 'Hello, ' + name + ' how are you doing today?'
 }
+
+062
+function getRealFloor(n) {
+    if (n > 12) {
+        return n - 2;
+    }
+    if(n > 0) {
+        return n - 1;
+    }
+    return n;
+}
