0dff2eca54776715af351b8bb91205575607610d
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-12-21 07:05:34 +0100

    64 codewars done!

--- 07.js
+++ 07.js
@@ -25,3 +25,12 @@ function oddOrEven(array) {
     }
     return 'odd';
 }
+
+064
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    for (years; p0 < p; years++) {
+        p0 += p0 * (percent / 100) + aug;
+    }
+    return years;
+}
